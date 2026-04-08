fcde6939fc51d2f1bf9e525fcb764ed1a2bbfb58
José William <prof.williamfirmino@gmail.com>
2026-04-08 09:50:37 -0300

    Alterações do dia 08/04/2026

--- app/components/ButtonForm.jsx
+++ app/components/ButtonForm.jsx
@@ -1,17 +1,19 @@
-import { Pressable, StyleSheet, Text } from "react-native"; 
+import { Pressable, StyleSheet, Text } from "react-native";
 
 // Declaração das constantes para estilização
 const BG_BUTTON = "#00213D"; 
 const TEXT_PRIMARY = "#FFFFFF"
 
 // Exportação do componente de reuso
-export default function Button() {
+export default function ButtonForm({ textButton}) {
     return (
-        <Pressable>
+        <Pressable style={styles.button}>
             <Text>
-                ENTRAR
+                { textButton ? textButton : "Não informado"}
+                
             </Text>
         </Pressable>
+        
     )
 }
 
--- app/screens/LoginScreen.jsx
+++ app/screens/LoginScreen.jsx
@@ -1,23 +1,36 @@
-import { ScrollView, View } from "react-native";
-import LabeledInput from "../components/LabeledInput";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import ButtonForm from "../components/ButtonForm";
+import LabeledInput from "../components/LabeledInput";
 
+// Componente de Tela
 export default function LoginScreen() {
-    return(
-        <SafeAreaView>
-            <ScrollView>
-            <LabeledInput 
-                label="Email" 
-                placeholder="Insira seu endereço de email"
-                keyboardType="email-adress"
-            />
-            <LabeledInput 
-                label="Senha" 
-                placeholder="Insira sua senha"
-                secureTextEntry={true}
-            />
-            </ScrollView>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView>
+        <Text style={styles.title}>Faça login em sua conta</Text>
+        <View style={styles.form}>
+          <LabeledInput
+            label="Email"
+            placeholder="Insira seu endereço de email"
+            keyboardType="email-adress"
+          />
+          <LabeledInput
+            label="Senha"
+            placeholder="Insira sua senha"
+            secureTextEntry={true}
+          />
+         <ButtonForm
+          
+         />
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  
+})
 
