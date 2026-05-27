12d74f4d762cc6d83da5f345d0642be977f7e6c2
José William <prof.williamfirmino@gmail.com>
2026-05-27 09:58:58 -0300

    Alterações parciais do dia 27/05/2026

---
+++ src/app/routes/StackRoutes.jsx
@@ -0,0 +1,17 @@
+// Arquivo que iremos montar as nossas rotas e configurar nossas telas
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterAll from "../screens/RegisterAll";
+import RegisterScreen from "../screens/RegisterScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function StackRoutes() {
+    return(
+        <Stack.Navigator>
+            <Stack.Screen name="login" component={LoginScreen}/>
+            <Stack.Screen name="registerall" component={RegisterAll}/>
+            <Stack.Screen name="register" component={RegisterScreen}/>
+        </Stack.Navigator>
+    )
+}
